9a9b2891a13ffdd7e7c3787a619cf203bdeccd73
kareem121004 <karimadel11112004@gmail.com>
2024-03-07 04:35:24 -0800

    commit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
 """
-script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-from fabric.api import run, put, env
-import os
+from fabric.api import put, run, env
+from os.path import exists
+
 
 env.hosts = ['54.157.160.23', '100.24.255.91']
 
 
 def do_deploy(archive_path):
-    """
-    distributes an archive to your web servers"""
-    if os.path.exists(archive_path) is False:
-        return(False)
+    """distributes an archive to the web servers"""
+    if not exists(archive_path):
+        print("Archive does not exist.")
+        return False
     try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        _filename = archive_path.split("/")[-1]
-        filename = _filename.split(".")[0]
-        run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
-            (_filename, filename))
-        run('rm /tmp/{}'.format(_filename))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename, filename))
-        run('rm -rf /data/web_static/releases/{}/web_static'
-            .format(filename))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'
-            .format(filename))
-        return(True)
-    except:
-        return(False)
+        run('sudo mkdir -p {}{}/'.format(path, no_ext))
+        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('sudo rm /tmp/{}'.format(file_n))
+        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('sudo rm -rf {}{}/web_static'.format(path, no_ext))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        if run('test -e /data/web_static/current').failed:
+            print("Failed to create symlink.")
+            return False
+        return True
+    except Exception as e:
+        print("An error occurred during deployment: {}".format(str(e)))
+        return False
